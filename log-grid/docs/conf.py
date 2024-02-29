96fd675386b304a5b6ad55ce69460fabed2e4587
Hippalectryon <hippalectryon@protonmail.com>
2024-02-29 10:24:23 +0100

    fix docs build

--- log-grid/docs/conf.py
+++ log-grid/docs/conf.py
@@ -34,8 +34,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     # 'sphinx.ext.autosectionlabel',
-    "IPython.sphinxext.ipython_console_highlighting",
-    "IPython.sphinxext.ipython_directive",
     "myst_parser",
     "autoapi.extension",
 ]
